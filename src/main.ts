181e3b6a96a2bbf0872552142e56b2d9c7fa6a66
eobcre <eobcre@gmail.com>
2023-07-15 19:45:12 -0400

    close pop up logic

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 import './style.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.style.display = 'block';
+
+  const app = document.getElementById('app');
+  const button = document.getElementById('button');
+
+  if (app && button) {
+    button.addEventListener('click', () => {
+      app.style.display = 'none';
+    });
+  }
+});
